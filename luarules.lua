58a2667d240d2db2682b1e10f045a333bc271cb6
Johan Lindberg <johan.lindberg@pulp.se>
2020-07-10 22:57:49 +0200

    Started working on the fact list

--- luarules.lua
+++ luarules.lua
@@ -1,2 +1,11 @@
 -- Lua Rules : A forward chaining rete rules engine
 
+local M = {}
+
+M.fact_list = {}
+M.assert = function (fact)
+    table.insert(M.fact_list, fact)
+    return true
+end
+
+return M
--- tests.lua
+++ tests.lua
@@ -10,4 +10,13 @@ function testLuaRulesModule()
     lu.assertIsTable(require('luarules'))
 end
 
+function testLuaRulesFactList()
+    local lr = require('luarules')
+
+    lu.assertEquals(#lr.fact_list, 0) -- ensure the fact-list is empty
+    lu.assertTrue(lr.assert({})) -- asserting an empty table should return True
+    lu.assertEquals(#lr.fact_list, 1)
+    
+end
+
 os.exit(lu.LuaUnit.run())
