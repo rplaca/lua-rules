191c30f5a01911e209b3e34dc4db132f72275674
Johan Lindberg <johan.lindberg@pulp.se>
2020-07-11 21:08:22 +0200

    Starting to move towards an actual fact-list implementation.
    Guessing it will be a table, hoping it will be fast. Not sure if I should bother
    about the f-1, f-2, ... etc indexing that is provided by CLIPS or if object
    references will be enough.

--- luarules.lua
+++ luarules.lua
@@ -3,8 +3,17 @@
 local M = {}
 
 M.fact_list = {}
-M.assert = function (fact)
-    table.insert(M.fact_list, fact)
+M.assert = function (...)
+    args = {...}
+    for _, fact in ipairs(args) do
+        table.insert(M.fact_list, fact)
+    end
+    
+    return true
+end
+
+M.retract = function (fact)
+    table.remove(M.fact_list, 1)
     return true
 end
 
--- tests.lua
+++ tests.lua
@@ -15,8 +15,13 @@ function testLuaRulesFactList()
 
     lu.assertEquals(#lr.fact_list, 0) -- ensure the fact-list is empty
     lu.assertTrue(lr.assert({})) -- asserting an empty table should return True
-    lu.assertEquals(#lr.fact_list, 1)
+    lu.assertEquals(#lr.fact_list, 1) -- and the fact_list should have 1 fact
+
+    lu.assertTrue(lr.retract({})) -- retracting the fact should return True
+    lu.assertEquals(#lr.fact_list, 0) -- and the fact_list should be empty again
     
+    lu.assertTrue(lr.assert({}, {}, {})) -- asserting three facts should work
+    lu.assertEquals(#lr.fact_list, 3) -- and the fact_list should be three items long
 end
 
 os.exit(lu.LuaUnit.run())
